5e453e1d0ad854d7eab5bc73436efeb3dfea259d
Bungas6666 <165682918+Bungas6666@users.noreply.github.com>
2024-04-01 13:18:36 -0300

    Add files via upload

---
+++ chamar_lousa.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LousaBranca from "./LousaBranca";
+
+function App() {
+  return (
+    <div className="App">
+      <LousaBranca />
+    </div>
+  );
+}
+
+export default App;
